50df7264e4f98abf2355c2a374289e776a39ee39
tmsundaram <sundaram.green4@gmail.com>
2020-06-12 14:29:51 +0530

    feature: rotate key on failure#6

--- trading_fetch_rate/fetch_rate.sh
+++ trading_fetch_rate/fetch_rate.sh
@@ -291,6 +291,7 @@ function do-op() {
 	##Retrive access_key and fetch data
 	local KEY_CNT=$(wc -l $KEY_FILE|awk '{print $1}')
 	local J=1
+	local ROTATE_BAL=$KEY_CNT
 	while [ $J -le $KEY_CNT ];
 	do
 		local COUNTER_VALUE=$(cat $COUNTER_FILE)
@@ -313,7 +314,7 @@ function do-op() {
 							;;
 						104) counter_action rotate $J
 							 J=$?
-							 ROTATE_BAL=$(echo "$KEY_CNT - 1" | bc -l)
+							 ROTATE_BAL=$(echo "$ROTATE_BAL - 1" | bc -l)
 							 if [ $ROTATE_BAL -eq "0" ]; then
 								log_msg "$FUNC" "(action:rotate) all keys reacehd request limit, no more keys to try"
 								J=$KEY_CNT
